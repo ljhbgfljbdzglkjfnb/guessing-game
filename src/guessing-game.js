aa890c215842f879610328845e6938e16409d0f4
lustrochka <94057960+ljhbgfljbdzglkjfnb@users.noreply.github.com>
2022-08-15 21:38:30 +0400

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    this.value = null;
+      this.min = null;
+      this.max = null;}
 
     setRange(min, max) {
-
+      this.max = max;
+      this.min = min;
     }
 
     guess() {
-
+      this.value = Math.round((this.max-this.min)/2)+this.min;
+      return this.value
     }
 
     lower() {
-
+      this.max = this.value
     }
 
     greater() {
-
+      this.min = this.value
     }
 }
 
